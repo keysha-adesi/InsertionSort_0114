27773473de7a80f630bf0f656737c5afed09fcb8
keysha-adesi <keyshaputrii590@gmail.com>
2026-03-12 11:09:46 +0700

    first commit

---
+++ Insertion Sort/Insertion_Sort.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];       //membuat panjang data 20
+int n;             //membuat variable input n
+
+void input (){ //procedure input
+    while (true){
+        cout << "masukan jumlah data pada array : ";
+        cin >> n;
+
+        if  (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+            
+        } 
+    }
+    cout << endl;
+    cout << "==========================================";
+    cout << "==========masukan Element Array===========";
+    cout << "==========================================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "data ke-"<< (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionsort()
+{ // procedur insertion sort
+    int temp;
+    int j;
+
+    for (int i =0; i < n; i++)
+    {
+        temp =arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nstep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+    
+}
+
+void display(){
+    cout << endl;
+    cout << "\n==============";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=============="
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+int main(){
+    input();
+    indertionsort();
+    display();
+    system("pause");
+    
+}
